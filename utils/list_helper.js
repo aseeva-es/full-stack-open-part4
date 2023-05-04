ea93b3386046f787b04ba106d2a6c530bd55f297
Elena Aseeva <aseeva.e.s@gmail.com>
2023-05-04 22:08:25 +0300

    4.4: helper functions and unit tests, step2 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,14 @@
-const dummy = () => {
-    return 1;
-  }
-  
-  module.exports = {
-    dummy
-  }
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  let total = blogs.reduce((prev, cur) => prev + cur.likes, 0);
+  console.log("res", total);
+  return total;
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+};
