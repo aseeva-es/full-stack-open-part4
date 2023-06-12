1ba74865da70f85e36c7ca24d1861dbf7330b6e0
Elena Aseeva <aseeva.e.s@gmail.com>
2023-06-12 21:43:31 +0300

    4.9: Blog list tests, step2 done

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,20 +1,47 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Blog =require('../models/blog')
 
 const api = supertest(app);
 
-test('bloges are returned as json', async ()=>{
+const initialBlogs = [
+    {
+      content: 'First blog',
+      likes: 5,
+      id: 0
+    },
+    {
+      content: 'Second blog',
+      likes: 8,
+      id: 1
+    },
+  ]
+
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    let noteObject = new Blog(initialBlogs[0])
+    await noteObject.save()
+    noteObject = new Blog(initialBlogs[1])
+    await noteObject.save()
+  })
+
+test('blogs are returned as json', async ()=>{
     await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type',/application\/json/)
 }, 10000)
 
-test('there are two blogs', async ()=> {
+test('all blogs are returned', async () =>{
     const response = await api.get('/api/blogs')
-    console.log('response: ',response.body)
-    expect(response.body).toHaveLength(4)
+    expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('unique identifier property of the blog posts is named id', async () =>{
+    const response = await api.get('/api/blogs')
+    console.log(response.body)
+    response.body.forEach((i)=>expect(i.id).toBeDefined())
 })
 
 afterAll(async () => {
